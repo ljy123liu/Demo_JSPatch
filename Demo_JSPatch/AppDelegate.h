e6ac7d4c6f24a312a5000b27136c08191aaa1322
lauyung <ljy123liu@gmail.com>
2016-05-26 17:53:32 +0800

    Initial Commit

---
+++ Demo_JSPatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_JSPatch
+//
+//  Created by LIUYONG on 16/5/26.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_JSPatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_JSPatch
+//
+//  Created by LIUYONG on 16/5/26.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
